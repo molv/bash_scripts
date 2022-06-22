af41a21c87441e04c74a000390267db0a9cfe873
molv <molvinec@gmail.com>
2022-06-22 22:49:07 +0300

    -

--- bacula_check.sh
+++ bacula_check.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-LOG=/var/log//bacula_check.log
+LOG=/var/log/bacula_check.log
 #checking availability of bacula console
 /usr/sbin/bconsole <<< 'version' > /dev/null 2>&1
 
@@ -11,8 +11,8 @@ echo "`date +"%Y.%m.%d-%H:%M:%S"`: Bacula OK" >> $LOG
 else
 
 echo "`date +"%Y.%m.%d-%H:%M:%S"`: Restarting bacula..." >> $LOG
-/etc/init.d/bacula-dir restart
-/etc/init.d/bacula-sd restart
+/etc/init.d/bacula-dir restart > /dev/null 2>&1
+/etc/init.d/bacula-sd restart > /dev/null 2>&1
 fi
 #clear log is more than 1 mb
 [[ $(find $LOG -type f -size +1M 2>/dev/null) ]] && echo  'Truncating LOG file' > $LOG
